850865299c5bfeb342d91b59ede954ab1c32b3e6
Shashank-hudakar <shambhulingappahudakar@gmail.com>
2025-05-18 10:34:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Products from "./components/Products";
+import Products from "./components/products";
 import { ThemeProvider } from "./context/ThemeContext";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
